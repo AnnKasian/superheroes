25ec41200217800f6999489fbb6246819da27b55
Anna Kasian <ann.kasian.ak@gmail.com>
2024-10-26 20:46:05 +0300

    + frontend constants

---
+++ apps/frontend/src/libs/constants/back-url.constant.ts
@@ -0,0 +1,3 @@
+const BACK_URL = -1;
+
+export { BACK_URL };
---
+++ apps/frontend/src/libs/constants/constants.ts
@@ -0,0 +1,5 @@
+export { BACK_URL } from "./back-url.constant.js";
+export { ICON_SIZE } from "./icon-size.constant.js";
+export { PAGE_INCREMENT } from "./page-increment.constant.js";
+export { EMPTY_SIZE } from "@superheroes/shared";
+export { FIRST_PAGE } from "~/libs/hooks/use-pagination/libs/constants/constants.js";
